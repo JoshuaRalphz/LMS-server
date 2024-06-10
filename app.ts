1cfc41605c1b81df1b78bef25705a4585076c97a
JoshuaRalphz <solomonjoshua14040@gmail.com>
2024-06-10 23:11:54 +0800

    message

--- app.ts
+++ app.ts
@@ -21,7 +21,7 @@ app.use(cookieParser());
 // cors => cross origin resource sharing
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://learn-hub-c-lient.vercel.app"],
+    origin: ["http://localhost:3000", "https://learnhub-c.vercel.app"],
     credentials: true,
   })
 );
